d589c7537cd753238edf45566fdb1648bdde4983
Lorenzo <45019206+lorenzomighie@users.noreply.github.com>
2019-12-16 12:56:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 from model_3dgan import GAN3D
 
 with tf.Session() as sess:
-    train_data_path = 'dataset/chair'
+    train_data_path = 'dataset/sofa'
     checkpoint_dir = 'checkpoints'
-    sample_g_path = 'generated/chair2'  # should be for the test
+    sample_g_path = 'generated/sofa'  # should be for the test
 
     gan3d = GAN3D(sess=sess, data_set_path=train_data_path,
                    checkpoint_dir=checkpoint_dir, sample_g_path=sample_g_path)
